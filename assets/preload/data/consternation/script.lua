c5e93ab00744d6ca3a4cc438c7ed66b4b663cdc5
ARAS AKGÜL <94735664+aras123457@users.noreply.github.com>
2022-02-13 11:47:08 +0300

    Add files via upload

---
+++ assets/preload/data/consternation/script.lua
@@ -0,0 +1,4 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'signDeath'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'BF_Deathsound'); --put in mods/sounds/
+end
