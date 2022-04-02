51a1a9c9c9901538a6b01b3a113a849091f2aa61
ARAS AKGÜL <94735664+aras123457@users.noreply.github.com>
2022-04-02 10:01:37 +0300

    Add files via upload

--- assets/preload/data/consternation/script.lua
+++ assets/preload/data/consternation/script.lua
@@ -1,4 +1,10 @@
-function onCreate()
-	setPropertyFromClass('GameOverSubstate', 'characterName', 'signDeath'); --Character json file for the death animation
-	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'BF_Deathsound'); --put in mods/sounds/
+local allowCountdown = true
+function onStartCountdown()
+	if not allowCountdown and isStoryMode and not seenCutscene then --Block the first countdown
+		setProperty('inCutscene', false);
+		startVideo('ughCutscene');
+		allowCountdown = false;
+		return Function_Stop;
+	end
+	return Function_Continue;
 end
